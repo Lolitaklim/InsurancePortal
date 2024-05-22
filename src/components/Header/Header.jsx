b92731629b7fc69a00591046b3cb4373973c1353
Lolitaklim <lolita.klim@yandex.com>
2024-05-22 20:12:35 +0400

    header desktop

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,100 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './Header.module.css'
 import Logo from './Logo'
-import { IoMenu } from 'react-icons/io5'
+import { IoMenu, IoClose } from 'react-icons/io5'
+import { useClickOutside } from '../../hooks/useClickOutside'
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  console.log(isOpen)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const menuRef = useRef(null)
+  useClickOutside(menuRef, () => {
+    if (isOpen) setTimeout(() => setIsOpen(false), 50)
+  })
+
+  const [itemId, setItemId] = useState(0)
+
+  useEffect(() => {
+    const runner = document.getElementById('runner')
+    const items = document.querySelectorAll(`.${styles.nav_item}`)
+    const header = document.querySelector('header')
+
+    const runnerMove = (item) => {
+      runner.style.left = item.offsetLeft + 'px'
+      runner.style.width = item.offsetWidth + 'px'
+    }
+
+    const mouseMove = ({ target }) => {
+      if (target.classList.contains(styles.nav_item)) runnerMove(target)
+      else runnerMove(items[itemId])
+    }
+
+    runnerMove(items[itemId])
+    header.addEventListener('mousemove', mouseMove)
+    return () => header.removeEventListener('mousemove', mouseMove)
+  }, [itemId])
 
   return (
-    <header className={styles.header_container}>
+    <header className={styles.header_container} id="header">
       <div className={`${styles.header} container`}>
         <Logo />
-        <nav className={`${styles.nav} ${isOpen ? styles.active : ""}`}>
-          <NavLink to="." end className={styles.nav_item}>
+        
+        <nav
+          className={`${styles.nav} ${isOpen ? styles.active : ''}`}
+          ref={menuRef}
+        >
+          <span className={styles.runner} id="runner"></span>
+          <button
+            className={styles.button_close}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <IoClose className={styles.close_icon} />
+          </button>
+          <NavLink
+            to="."
+            end
+            className={styles.nav_item}
+            onClick={() => setItemId(0)}
+          >
             ГЛАВНАЯ
           </NavLink>
-          <NavLink to="insurance" className={styles.nav_item}>
+          <NavLink
+            to="insurance"
+            className={styles.nav_item}
+            onClick={() => setItemId(1)}
+          >
             СТРАХОВАНИЕ
           </NavLink>
-          <NavLink to="how-we-work" className={styles.nav_item}>
+          <NavLink
+            to="how-we-work"
+            className={styles.nav_item}
+            onClick={() => setItemId(2)}
+          >
             КАК РАБОТАЕМ
           </NavLink>
-          <NavLink to="useful-info" className={styles.nav_item}>
+          <NavLink
+            to="useful-info"
+            className={styles.nav_item}
+            onClick={() => setItemId(3)}
+          >
             ПОЛЕЗНОЕ
           </NavLink>
-          <NavLink to="contacts" className={styles.nav_item}>
+          <NavLink
+            to="contacts"
+            className={styles.nav_item}
+            onClick={() => setItemId(4)}
+          >
             КОНТАКТЫ
           </NavLink>
         </nav>
         <div className={styles.mobile_menu}>
-          <span>Меню |</span>
-          <button className={styles.button} onClick={() => setIsOpen(!isOpen)}>
-            <IoMenu />
+          <span>МЕНЮ |</span>
+          <button
+            className={styles.button_menu}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <IoMenu className={styles.button_menu_icon} />
           </button>
         </div>
       </div>
