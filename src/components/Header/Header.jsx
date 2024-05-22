2886803228b069d0cc53bd5de86cb765c8395cbf
Lolitaklim <lolita.klim@yandex.com>
2024-05-22 04:23:57 +0400

    logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,42 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './Header.module.css'
+import Logo from './Logo'
+import { IoMenu } from 'react-icons/io5'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen)
+
   return (
-    <div className={styles.header}>
-      <p>ff</p>
-      <nav>
-        <NavLink to="." end>
-          home
-        </NavLink>
-        <br />
-        <NavLink to="insurance">insurance</NavLink>
-        <br />
-        <NavLink to="how-we-work">HowWeWork</NavLink>
-        <br />
-        <NavLink to="useful-info">useful-info</NavLink>
-        <br />
-        <NavLink to="contacts">контакты contacts</NavLink>
-        <br />
-      </nav>
-    </div>
+    <header className={styles.header_container}>
+      <div className={`${styles.header} container`}>
+        <Logo />
+        <nav className={`${styles.nav} ${isOpen ? styles.active : ""}`}>
+          <NavLink to="." end className={styles.nav_item}>
+            ГЛАВНАЯ
+          </NavLink>
+          <NavLink to="insurance" className={styles.nav_item}>
+            СТРАХОВАНИЕ
+          </NavLink>
+          <NavLink to="how-we-work" className={styles.nav_item}>
+            КАК РАБОТАЕМ
+          </NavLink>
+          <NavLink to="useful-info" className={styles.nav_item}>
+            ПОЛЕЗНОЕ
+          </NavLink>
+          <NavLink to="contacts" className={styles.nav_item}>
+            КОНТАКТЫ
+          </NavLink>
+        </nav>
+        <div className={styles.mobile_menu}>
+          <span>Меню |</span>
+          <button className={styles.button} onClick={() => setIsOpen(!isOpen)}>
+            <IoMenu />
+          </button>
+        </div>
+      </div>
+    </header>
   )
 }
 
